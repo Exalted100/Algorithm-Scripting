ed1ef31ebe5fc5a76a860a0af0118c586ca7bbfa
Fawaz Haroun <harounayomikun@gmail.com>
2021-02-01 20:59:03 +0100

    completed the time conversion algorithm

--- timeConversion.js
+++ timeConversion.js
@@ -5,3 +5,31 @@ Complete the timeConversion function in the editor below. It should return a new
 timeConversion has the following parameter(s):
 
 string s: a time in 12 hour format */
+
+function timeConversion(s) {
+    /*
+     * Write your code here.
+     */
+    const timeArr = s.split("")
+    
+    let mutatedArr = [...timeArr]
+    mutatedArr.shift()
+    mutatedArr.shift()
+    
+    let newArr = [timeArr[0] + timeArr[1], ...mutatedArr]
+    
+    if (timeArr[timeArr.length - 2] === "A" && newArr[0] === "12") {
+        mutatedArr.pop()
+        mutatedArr.pop()
+        return ["00", ...mutatedArr].join("")
+    } else if (timeArr[timeArr.length - 2] === "A") {
+        timeArr.pop()
+        timeArr.pop()
+        return [timeArr.pop()].join("")
+    } else {
+        mutatedArr.pop()
+        mutatedArr.pop()
+        return [+newArr[0] + 12, ...mutatedArr].join("")
+    }
+
+}
